98df42ef097b6e1bf74489cb9ad3f6295fa62d74
Anderson Lozada <andersonlozada@quadi.co>
2024-03-31 02:38:26 -0500

    feat: :art: initial data

---
+++ src/data/language.ts
@@ -0,0 +1,18 @@
+export const Language = [
+    {
+      id: '1',
+      icon:'',
+      language: 'React',
+      tecnology: 'Frontend',
+      ability: 'Vite',
+      specification:''
+    },
+    {
+        id: '2',
+        icon:'',
+        language: 'React',
+        tecnology: 'Frontend',
+        ability: 'rxjs',
+        specification:''
+      },
+  ];
